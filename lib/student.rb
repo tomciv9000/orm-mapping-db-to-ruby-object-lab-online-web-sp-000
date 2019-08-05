0e953ff1a6def5b2c7d469ac7827bff2c7316593
Tom White <eugene.white@gmail.com>
2019-08-05 00:16:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,12 +3,10 @@ class Student
 
   def self.new_from_db(row)
     new_student = self.new
-    new_song.id = row[0]
-    new_song.name =  row[1]
-    new_song.length = row[2]
-    new_song
-    
-    # create a new Student object given a row from the database
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
