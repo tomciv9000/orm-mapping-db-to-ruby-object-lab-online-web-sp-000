49983a55b71b6c6bb009dc148d75fec776702cd0
Tom White <eugene.white@gmail.com>
2019-08-05 00:21:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,6 +17,7 @@ class Student
  
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
@@ -57,4 +58,5 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
 end
