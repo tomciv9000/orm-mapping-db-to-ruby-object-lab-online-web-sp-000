082d6e40d24ee6a87792a632555fac9c7e8d703a
Tom White <eugene.white@gmail.com>
2019-08-05 00:31:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,6 +56,19 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
