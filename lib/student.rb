2f5dfb3bcf23b405e27f75796f0a8acecb9c2bad
Tom White <eugene.white@gmail.com>
2019-08-05 00:29:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,12 +48,13 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade < 10
+      WHERE grade = 10
       LIMIT ?
     SQL
     
     DB[:conn].execute(sql, n).map do |row|
       self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
